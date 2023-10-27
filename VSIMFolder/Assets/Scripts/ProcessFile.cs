9b9ac0e112154e9ec22af5466c04a11ef430d3cf
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-10-27 09:32:50 +0100

    Continuing work on file reading

--- VSIMFolder/Assets/Scripts/ProcessFile.cs
+++ VSIMFolder/Assets/Scripts/ProcessFile.cs
@@ -1,27 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProcessFile : MonoBehaviour
 {
-    DirectoryInfo dir = new DirectoryInfo("D:\\Github Clones\\VSIM-Folder\\VSIMFolder\\Assets\\Height Data");
 
-    FileInfo inputFile = new FileInfo("merged.txt");
+    int xMin;
+    int xMax;
+    int zMin;
+    int zMax;
+
+    string filePath = @"D:\Github Clones\VSIM-Folder\VSIMFolder\Assets\Height Data\merged.txt";
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(GetLinesCount(inputFile));
+        var lineCount = File.ReadLines(filePath).Count();
+        Debug.Log(lineCount);
+        FindMinMax(lineCount);
     }
 
-    long GetLinesCount(FileInfo file)
+    void FindMinMax(int fileLength)
     {
-        long x = File.ReadLines(file.FullName).Count();
-        return x;
+        string line;
+        Vector3 temp = new Vector3();
+        StreamReader read = new StreamReader(filePath);
+
+        for (int i = 0; i < fileLength; i++)
+        {
+            line = read.ReadLine();
+            List<String> pointValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            temp = new Vector3(float.Parse(pointValues[0], CultureInfo.InvariantCulture.NumberFormat),
+                               float.Parse(pointValues[2], CultureInfo.InvariantCulture.NumberFormat),
+                               float.Parse(pointValues[1], CultureInfo.InvariantCulture.NumberFormat));
+            Debug.Log(temp);
+        }
     }
 }
