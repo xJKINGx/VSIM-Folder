2fb9c507c42068569c798722d9aa8723b6743437
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-10-27 11:03:05 +0100

    trying to optimize

--- VSIMFolder/Assets/Scripts/ProcessFile.cs
+++ VSIMFolder/Assets/Scripts/ProcessFile.cs
@@ -45,36 +45,46 @@ public class ProcessFile : MonoBehaviour
     void ConvertData(int fileLength)
     {
         string line;
-
+        
         StreamReader read = new StreamReader(inFilePath);
+
+        int a = 0;
 
         for (int i = 0; i < fileLength; i++)
         {
             line = read.ReadLine();
-            List<String> pointValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList<string>();
-            
-            // To change the x- and z-values we need to convert to float
-            List<float> fPointValues = new List<float>();
-            fPointValues.Add(float.Parse(pointValues[0], CultureInfo.InvariantCulture.NumberFormat) - 611000.0f);
-            fPointValues.Add(float.Parse(pointValues[2], CultureInfo.InvariantCulture.NumberFormat));
-            fPointValues.Add(float.Parse(pointValues[1], CultureInfo.InvariantCulture.NumberFormat) - 6641000.0f);
 
-            // And then convert it back into strings
-            pointValues[0] = fPointValues[0].ToString();
-            pointValues[1] = fPointValues[1].ToString();
-            pointValues[2] = fPointValues[2].ToString();
-            
-            // Clearing the list to save memory
-            fPointValues.Clear();
-            
-            // Creating the output string which will be one new line on the file
-            string outputString = pointValues[0] + " " + pointValues[2] + " " + pointValues[1];
-
-            using (StreamWriter w = File.AppendText(outFilepath))
+            if (a >= 100)
             {
-                w.WriteLine(outputString);
+                List<String> pointValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+                
+                // To change the x- and z-values we need to convert to float
+                List<float> fPointValues = new List<float>();
+                fPointValues.Add(float.Parse(pointValues[0], CultureInfo.InvariantCulture.NumberFormat) - 611000.0f);
+                fPointValues.Add(float.Parse(pointValues[2], CultureInfo.InvariantCulture.NumberFormat));
+                fPointValues.Add(float.Parse(pointValues[1], CultureInfo.InvariantCulture.NumberFormat) - 6641000.0f);
+
+                // And then convert it back into strings
+                pointValues[0] = fPointValues[0].ToString();
+                pointValues[1] = fPointValues[1].ToString();
+                pointValues[2] = fPointValues[2].ToString();
+                
+                // Clearing the list to save memory
+                fPointValues.Clear();
+                
+                // Creating the output string which will be one new line on the file
+                string outputString = pointValues[0] + " " + pointValues[2] + " " + pointValues[1];
+
+                using (StreamWriter w = File.AppendText(outFilepath))
+                {
+                    w.WriteLine(outputString);
+                } 
+                a = 0;
             }
-            
+            else
+            {
+                a++;
+            }
         }
     }
 }
